f16239b042dd6819325a83195a2dea528e940c39
deanwu <wowdeanwu@163.com>
2016-01-06 18:34:23 +0800

    add the redis

--- server/routes.js
+++ server/routes.js
@@ -11,7 +11,8 @@ module.exports = function(app) {
 
   // Insert routes below
   app.use('/api/configs', require('./api/config'));
-  
+   app.use('/api/redis', require('./api/redis'));
+
   // All undefined asset or api routes should return a 404
   app.route('/:url(api|auth|components|app|bower_components|assets)/*')
    .get(errors[404]);
